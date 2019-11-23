d6eb1363f51503e1798d15c05e389003ebbddf57
Yurii Horoshko <yuriy.horoshkolv@gmail.com>
2019-11-23 18:04:41 +0200

    Contract update

--- Contract/Contract/ByteDefinition/Bytes.cs
+++ Contract/Contract/ByteDefinition/Bytes.cs
@@ -9,13 +9,15 @@ namespace Contract.ByteDefinition
             ["connection_request"] = new byte[] {101},
             ["connection_response"] = new byte[] {111},
             ["size_accepted"] = new byte[] {155},
-            ["matrix_accepted"] = new byte[] {156}
+            ["matrix_accepted"] = new byte[] {156},
+            ["progress_request"] = new byte[] {127}
         };
 
         public static Dictionary<string, byte> byteDef = new Dictionary<string, byte>()
         {
             ["task_prefix"] = (byte) 153,
             ["size_prefix"] = (byte) 112,
+            ["percent_prefix"] = (byte) 113
             
         };
     }
